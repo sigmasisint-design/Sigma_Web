3131397908aa87079c779ce9292f6b3dfa472397
sigmasisint-design <sigma.sisint@gmail.com>
2025-10-04 08:08:14 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,25 @@
 
+// pages/index.js
+import React from 'react';
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Bem-vindo ao Meu Projeto Modular!</h1>
+      <p>Esta é a sua nova Home Page ( / ).</p>
+      
+      <p>Acesse a nova tela de usuários:</p>
+      {/* O Next.js usa o componente Link para navegação */}
+      <Link href="/usuarios">
+        Ir para a Tela de Usuários
+      </Link>
+      
+      {/*
+        COPIE E COLE A PARTE DO SEU CÓDIGO HTML/JSX/JS
+        que era a Home Page (a tela que aparecia no /) do seu index original.
+      */}
+      
+    </div>
+  );
+}
